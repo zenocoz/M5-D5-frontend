1d0541ef89b8d1bc438117bbe824e0886a80c529
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-12-11 15:37:48 +0100

    Update SingleProduct.jsx

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -4,7 +4,7 @@ import { Card, Button } from "react-bootstrap";
 function SingleProduct(props) {
   return (
     <Card>
-      <Card.Img variant="top" src={props.productObj.image} />
+      <Card.Img variant="top" src={props.productObj.imageUrl} />
       <Card.Body>
         <Card.Title>{props.productObj.name}</Card.Title>
         <Card.Text>{props.productObj.description}</Card.Text>
