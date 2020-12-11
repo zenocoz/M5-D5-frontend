9d8aee2224e1e9ea9ad505826c99e19ee00121b2
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-12-11 15:00:01 +0100

    almost finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Container, Row, Col } from "react-bootstrap";
+import SingleProduct from "./SingleProduct";
 class Home extends React.Component {
   state = {
     products: [],
@@ -12,12 +13,38 @@ class Home extends React.Component {
       let response = await fetch(
         `http://localhost:${process.env.REACT_APP_PORT}/products`
       );
+
+      let products = await response.json();
+      console.log(products);
+
+      if (response.ok) {
+        this.setState({ products, loading: false });
+      } else {
+        alert("something went wrong");
+        this.setState({ loading: false });
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
+  componentDidMount = () => {
+    this.fetchProducts();
+  };
   render() {
-    return <></>;
+    return (
+      <>
+        <Container>
+          <Row className="mt-5">
+            {this.state.products.map((product, index) => (
+              <Col md={3} key={index}>
+                <SingleProduct productObj={product} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </>
+    );
   }
 }
 
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
-function SingleProduct() {
+function SingleProduct(props) {
   return (
     <Card>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={props.productObj.image} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{props.productObj.name}</Card.Title>
+        <Card.Text>{props.productObj.description}</Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
